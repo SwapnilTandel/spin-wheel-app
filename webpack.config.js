d7f18b098e7267c5cd8eb1f17389f0b03cf840a5
Swapnil T <swaptan@icloud.com>
2025-10-09 21:34:59 -0400

    Revert "Fix webpack publicPath to resolve bundle loading issue"
    This reverts commit 4948746154517e70df3b354591f758ed0f6c0beb.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/spin-wheel-app/'
   },
   plugins: [
     new HtmlWebpackPlugin({
