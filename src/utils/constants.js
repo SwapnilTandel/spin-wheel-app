e1ed4749f88f44e3691634d2012ed81c9d582efd
Swapnil T <swaptan@icloud.com>
2025-10-09 20:17:11 -0400

    Update constants configuration
    - Modified src/utils/constants.js with latest configuration updates
    - Updated application constants and settings

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -227,17 +227,17 @@ export const DEFAULT_CATEGORIES = {
     { id: 11, name: 'Parle G 10Pcs', color: '#228B22', image: null, number: 100 }, // Forest Green
   ],
   100: [
-    { id: 1, name: '$300', color: '#FF6B35', image: null, number: 1 }, // Diwali Orange
-    { id: 2, name: '$50', color: '#FFD700', image: null, number: 10 }, // Gold
-    { id: 3, name: '$25', color: '#4B0082', image: null, number: 20 }, // Indigo
-    { id: 4, name: '$10', color: '#FF1493', image: null, number: 30 }, // Deep Pink
-    { id: 5, name: 'Olivelila Oil', color: '#32CD32', image: null, number: 40 }, // Lime Green
-    { id: 6, name: 'Any Basmati Rice - 10 LB', color: '#FF8C00', image: null, number: 50 }, // Deep Orange
-    { id: 7, name: 'Rajbhog Dahi - 4 LB', color: '#00BFFF', image: null, number: 60 }, // Deep Sky Blue
-    { id: 8, name: 'Maharaja Brand Almond - 3 LB', color: '#FFA500', image: null, number: 70 }, // Saffron
-    { id: 9, name: 'Any Bakery Product - $10', color: '#8A2BE2', image: null, number: 80 }, // Blue Violet
-    { id: 10, name: 'Deep Tandoori Naan - Family PK', color: '#FF6347', image: null, number: 90 }, // Tomato
-    { id: 11, name: 'Better Luck Next Time', color: '#228B22', image: null, number: 100 }, // Forest Green
+    { id: 1, name: 'Laxmi Moong Dal 4LB', color: '#FF6B35', image: null, number: 80 }, // Diwali Orange
+    { id: 2, name: '$10', color: '#FFD700', image: null, number: 10 }, // Gold
+    { id: 3, name: 'Maharaja Almonds 3LB', color: '#4B0082', image: null, number: 20 }, // Indigo
+    { id: 4, name: '$25', color: '#FF1493', image: null, number: 30 }, // Deep Pink
+    { id: 5, name: 'Olio villa Oil', color: '#32CD32', image: null, number: 40 }, // Lime Green
+    { id: 6, name: '$50', color: '#FF8C00', image: null, number: 50 }, // Deep Orange
+    { id: 7, name: 'Laxmi Basmati Rice 4LB', color: '#00BFFF', image: null, number: 60 }, // Deep Sky Blue
+    { id: 8, name: 'Wagh Bakri Tea 2LB', color: '#FFA500', image: null, number: 70 }, // Saffron
+    { id: 9, name: '$300', color: '#8A2BE2', image: null, number: 80 }, // Blue Violet
+    { id: 10, name: 'Wesson Oil 1Gallon', color: '#FF6347', image: null, number: 90 }, // Tomato
+    { id: 11, name: 'Shezan Mango Drink 1Case', color: '#228B22', image: null, number: 100 }, // Forest Green
   ]
 };
 
