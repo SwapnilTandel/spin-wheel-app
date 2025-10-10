b7cbfff2fe9ad29def3042062706a2efc7f03597
Swapnil T <swaptan@icloud.com>
2025-10-09 20:08:47 -0400

    Update constants file
    - Modified src/utils/constants.js
    - Updated configuration values

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -214,17 +214,17 @@ export const COLOR_OPTIONS = [
 
 export const DEFAULT_CATEGORIES = {
   50: [
-    { id: 1, name: '$200', color: '#FF6B35', image: null, number: 1 }, // Diwali Orange
-    { id: 2, name: '$20', color: '#FFD700', image: null, number: 10 }, // Gold
-    { id: 3, name: '$10', color: '#4B0082', image: null, number: 20 }, // Indigo
-    { id: 4, name: '$5', color: '#FF1493', image: null, number: 30 }, // Deep Pink
-    { id: 5, name: 'Sugar 4LB', color: '#32CD32', image: null, number: 40 }, // Lime Green
-    { id: 6, name: 'Maggie Masala - 700 gm', color: '#FF8C00', image: null, number: 50 }, // Deep Orange
-    { id: 7, name: 'Parle - G 10pcs', color: '#00BFFF', image: null, number: 60 }, // Deep Sky Blue
-    { id: 8, name: 'Yogurt 2LB', color: '#FFA500', image: null, number: 70 }, // Saffron
-    { id: 9, name: 'Bakery Product - $5', color: '#8A2BE2', image: null, number: 80 }, // Blue Violet
-    { id: 10, name: 'Deep Naan (1pk-5 pcs)', color: '#FF6347', image: null, number: 90 }, // Tomato
-    { id: 11, name: 'Better Luck Next Time', color: '#228B22', image: null, number: 100 }, // Forest Green
+    { id: 1, name: 'Shezan Mango Juice 250ml*6', color: '#FF6B35', image: null, number: 80 }, // Diwali Orange
+    { id: 2, name: '$5', color: '#FFD700', image: null, number: 10 }, // Gold
+    { id: 3, name: 'Vital 125 Tea Bag', color: '#4B0082', image: null, number: 70 }, // Indigo
+    { id: 4, name: '$10', color: '#FF1493', image: null, number: 30 }, // Deep Pink
+    { id: 5, name: 'Maharaja Brown Sugar 4LB', color: '#32CD32', image: null, number: 60 }, // Lime Green
+    { id: 6, name: '$20', color: '#FF8C00', image: null, number: 1 }, // Deep Orange
+    { id: 7, name: 'Nescafé Coffee 200GM', color: '#00BFFF', image: null, number: 60 }, // Deep Sky Blue
+    { id: 8, name: 'Shudh Basmati Rice 10LB', color: '#FFA500', image: null, number: 70 }, // Saffron
+    { id: 9, name: '$200', color: '#8A2BE2', image: null, number: 1 }, // Blue Violet
+    { id: 10, name: 'Maharaja Cashew 1.80LB', color: '#FF6347', image: null, number: 90 }, // Tomato
+    { id: 11, name: 'Parle G 10Pcs', color: '#228B22', image: null, number: 100 }, // Forest Green
   ],
   100: [
     { id: 1, name: '$300', color: '#FF6B35', image: null, number: 1 }, // Diwali Orange
