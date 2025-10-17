f176c58a28202c127e7742c0c8143f98ddee4e37
Swapnil T <swaptan@icloud.com>
2025-10-17 21:14:24 +0530

    Update constants.js file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -233,7 +233,7 @@ export const DEFAULT_CATEGORIES = {
     { id: 4, name: '$25', color: '#FF1493', image: null, number: 1 }, // Deep Pink
     { id: 5, name: 'Any Bakery Items Worth $10', color: '#32CD32', image: null, number: 80 }, // Lime Green
     { id: 6, name: '$50', color: '#FF8C00', image: null, number: 1 }, // Deep Orange
-    { id: 7, name: 'Laxmi Basmati Rice 10Lb', color: '#00BFFF', image: null, number: 80 }, // Deep Sky Blue
+    { id: 7, name: 'Shudh Basmati Rice 10Lb', color: '#00BFFF', image: null, number: 80 }, // Deep Sky Blue
     { id: 8, name: 'Soan Papdi Sweet 500GM', color: '#FFA500', image: null, number: 80 }, // Saffron
     { id: 9, name: '$300', color: '#8A2BE2', image: null, number: 1 }, // Blue Violet
     { id: 10, name: 'Maharaja Almonds 3Lb', color: '#FF6347', image: null, number: 80 }, // Tomato
