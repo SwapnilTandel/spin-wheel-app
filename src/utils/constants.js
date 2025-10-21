af50095acbe904a6df3e1e474a2fd78b3d111ea0
anuragak7 <anuragkalakanti7@gmail.com>
2025-10-21 12:53:37 -0400

    almonds 80 to 1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -236,7 +236,7 @@ export const DEFAULT_CATEGORIES = {
     { id: 7, name: 'Shudh Basmati Rice 10Lb', color: '#00BFFF', image: null, number: 80 }, // Deep Sky Blue
     { id: 8, name: 'Shudh Rasgulla 1KG', color: '#FFA500', image: null, number: 80 }, // Saffron
     { id: 9, name: '$300', color: '#8A2BE2', image: null, number: 1 }, // Blue Violet
-    { id: 10, name: 'Maharaja Almonds 3Lb', color: '#FF6347', image: null, number: 80 }, // Tomato
+    { id: 10, name: 'Maharaja Almonds 3Lb', color: '#FF6347', image: null, number: 1 }, // Tomato
     { id: 11, name: 'Maggi Noodles 560GM', color: '#228B22', image: null, number: 80 }, // Forest Green
   ]
 };
